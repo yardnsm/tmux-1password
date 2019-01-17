7a7e88178e36736f31f4b7e0d81b4fe46b2dc877
bluedrink9 <>
2019-01-17 16:37:10 +1300

    Remove accidentally-added debug file

--- scripts/filtertest.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-filter="'.[] | [select(.overview.URLs | map(select(.u == \"wiggle.co.nz\")) | length == 1)?] | map([ .overview.title, .uuid ] | join(\",\")) | .[] '"
-lpass show --json --expand-multi -G '.*' | jq $filter
