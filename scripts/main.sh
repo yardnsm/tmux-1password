f6b4a5065115634bc5e9966cf959a2d448681129
Yarden Sod-Moriah <yardnsm@gmail.com>
2022-03-17 22:47:16 +0200

    Initial working version with version 2

--- scripts/main.sh
+++ scripts/main.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
+# shellcheck disable=2155
 
 cd "$(dirname "${BASH_SOURCE[0]}")" \
   || exit 1
@@ -10,6 +11,7 @@ source "./spinner.sh"
 
 # ------------------------------------------------------------------------------
 
+declare -r EXPECTED_MIN_OP_CLI_VERSION="2.0.0"
 declare -r TMP_TOKEN_FILE="$HOME/.op_tmux_token_tmp"
 
 declare -r OPT_SUBDOMAIN="$(get_tmux_option "@1password-subdomain" "my")"
@@ -46,7 +48,7 @@ spinner_stop() {
 # ------------------------------------------------------------------------------
 
 op_login() {
-  op --cache signin "$OPT_SUBDOMAIN" --output=raw > "$TMP_TOKEN_FILE"
+  op signin --account="$OPT_SUBDOMAIN" --cache --session="$(op_get_session)" --force --raw > "$TMP_TOKEN_FILE"
   tput clear
 }
 
@@ -85,19 +87,25 @@ get_op_items() {
       .[]
       | [
           select(
-            (.templateUuid == \"001\" and (.overview.URLs | map(select(.u)) | length >= 1)) or
-            (.templateUuid == \"005\")
+            (.category == \"LOGIN\") or
+            (.category == \"PASSWORD\")
           )?
         ]
-      | map([ .overview.title, .uuid ]
+      | map([ .title, .id ]
       | join(\",\"))
       | .[]
     "
   fi
 
-  op --cache list items --vault="$OPT_VAULT" --session="$(op_get_session)" 2> /dev/null \
+  op item list \
+    --cache \
+    --format=json \
+    --categories="LOGIN,PASSWORD" \
+    --vault="$OPT_VAULT" \
+    --session="$(op_get_session)" \
+    2> /dev/null \
     | jq "$JQ_FILTER" --raw-output
-}
+  }
 
 get_op_item_password() {
   local -r ITEM_UUID="$1"
@@ -125,17 +133,24 @@ get_op_item_password() {
   #       }
 
   local -r JQ_FILTER="
-    .details
-    | if .password and .password != \"\" then
-        .password
-      else
-        .fields[]
-        | select (.designation == \"password\")
-        | .value
-      end
+      # For cases where we might get a single item - we always want to start with an array
+      [.] + [] | flatten
+
+      # Select the items whose purpose is... being a password
+      | map(select(.purpose == \"PASSWORD\"))
+
+      # Select the first one
+      | .[0]
+
+      # Return the value
+      | .value
     "
 
-  op --cache get item "$ITEM_UUID" --session="$(op_get_session)" \
+  op item get "$ITEM_UUID" \
+    --cache \
+    --fields type=concealed \
+    --format json \
+    --session="$(op_get_session)" \
     | jq "$JQ_FILTER" --raw-output
 }
 
@@ -143,6 +158,37 @@ get_op_item_totp() {
   local -r ITEM_UUID="$1"
 
   op --cache get totp "$ITEM_UUID" --session="$(op_get_session)"
+}
+
+verify_op_version() {
+  local op_version="$(op --version)"
+
+  utils::compare_semver "$op_version" "$EXPECTED_MIN_OP_CLI_VERSION"
+
+  if [[ $? -eq 2 ]]; then
+    display_message \
+      "1Password CLI version is not compatible with this plugin: ${op_version} < ${EXPECTED_MIN_OP_CLI_VERSION}"
+
+    return 1
+  fi
+
+  return 0
+}
+
+verify_signin() {
+  local connected_accounts_count="$(( $(op account list | wc -l) - 1 ))"
+
+  if [[ "$connected_accounts_count" -eq 0 ]]; then
+    utils::ask_for_confirmation "You haven't added any accounts to 1Password CLI. Would you like to add one now?"
+
+    if utils::answer_is_yes; then
+      op account add
+    else
+      return 1
+    fi
+  fi
+
+  op_login
 }
 
 # ------------------------------------------------------------------------------
@@ -162,30 +208,25 @@ main() {
     --bind "enter:execute(echo pass,{+})+abort"
     --bind "ctrl-u:execute(echo totp,{+})+abort")
 
+  # Check for version
+  if ! verify_op_version; then
+      return 0
+  fi
+
+  # Verify signin
+  if ! verify_signin; then
+    display_message "1Password CLI signin has failed"
+    return 0
+  fi
+
   spinner_start "Fetching items"
   items="$(get_op_items)"
   spinner_stop
-
-  if [[ -z "$items" ]]; then
-
-    # Needs to login
-    op_login
-
-    if [[ -z "$(op_get_session)" ]]; then
-      display_message "1Password CLI signin has failed"
-      return 0
-    fi
-
-    spinner_start "Fetching items"
-    items="$(get_op_items)"
-    spinner_stop
-  fi
 
   selected_item="$(echo "$items" | awk -F ',' '{ print $1 }' | fzf "${fzf_opts[@]}")"
 
   if [[ -n "$selected_item" ]]; then
     selected_item_name=${selected_item#*,}
-
     selected_item_uuid="$(echo "$items" | grep "^$selected_item_name," | awk -F ',' '{ print $2 }')"
 
     case ${selected_item%%,*} in
--- scripts/utils.sh
+++ scripts/utils.sh
@@ -46,3 +46,52 @@ clear_clipboard() {
     return 1
   fi
 }
+
+# Shamelessly taken from:
+# https://stackoverflow.com/questions/4023830/how-to-compare-two-strings-in-dot-separated-version-format-in-bash/4025065#4025065
+utils::compare_semver() {
+    if [[ $1 == $2 ]]; then
+        return 0
+    fi
+
+    local IFS="."
+    local i
+    local version_1="$1"
+    local version_2="$2"
+
+    # Fill empty fields in version_1 with zeros
+    for ((i=${#version_1[@]}; i<${#version_2[@]}; i++)); do
+        version_1[i]=0
+    done
+
+    for ((i=0; i<${#version_1[@]}; i++)); do
+        if [[ -z ${version_2[i]} ]]; then
+            # Fill empty fields in version_2 with zeros
+            version_2[i]=0
+        fi
+
+        if ((10#${version_1[i]} > 10#${version_2[i]})); then
+            return 1
+        fi
+
+        if ((10#${version_1[i]} < 10#${version_2[i]})); then
+            return 2
+        fi
+    done
+
+    return 0
+}
+
+utils::ask_for_confirmation() {
+  local question="$1"
+
+  printf "\\e[0;33m[?]\\e[0m %b [y/N] " "$question"
+  read -r -n 1
+  echo -ne "\n"
+}
+
+utils::answer_is_yes() {
+  [[ "$REPLY" =~ ^[Yy]$ ]] \
+    && return 0 \
+    || return 1
+}
